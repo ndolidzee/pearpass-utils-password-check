18095320eaa3f4b45805830a7438fdcb93046a48
ndolidze <n.dolidze@noxtton.com>
2025-05-07 22:23:05 +0400

    feat: Add error messages

--- src/passphrase.js
+++ src/passphrase.js
@@ -1,17 +1,19 @@
 /**
  * @param {Array<string>} wordsArray
- * @param {{ capitalLetters: boolean, symbols: boolean, numbers: boolean, words: number }} rulesConfig
- * @returns {{ isSafe: boolean, rules: Record<string, boolean> }}
+ * @param {{ rules: { capitalLetters?: boolean, symbols?: boolean, numbers?: boolean, words?: number }, errors?: Record<string, string> }} config
+ * @returns {{ isSafe: boolean, rules: Record<string, boolean>, errors: string[] }}
  */
-export const isPassphraseSafe = (wordsArray, rulesConfig = {}) => {
+export const isPassphraseSafe = (wordsArray, config = {}) => {
+  const { rules = {}, errors = {} } = config
+
   const {
     capitalLetters = true,
     symbols = true,
     numbers = true,
     words = 8
-  } = rulesConfig
+  } = rules
 
-  const rules = {
+  const rulesCheck = {
     minWords: wordsArray?.length >= words,
     uniqueWords:
       new Set(wordsArray.map((word) => word.replace(/[^a-zA-Z]/g, ''))).size ===
@@ -23,5 +25,14 @@ export const isPassphraseSafe = (wordsArray, rulesConfig = {}) => {
     numbers: numbers ? /\d/.test(wordsArray.join('')) : true
   }
 
-  return { isSafe: Object.values(rules).every(Boolean), rules: rules }
+  const failedRules = Object.keys(rulesCheck).filter(
+    (rule) => !rulesCheck[rule]
+  )
+  const errorMessages = failedRules.map((rule) => errors[rule])
+
+  return {
+    isSafe: failedRules.length === 0,
+    rules: rulesCheck,
+    errors: errorMessages
+  }
 }
--- src/password.js
+++ src/password.js
@@ -1,24 +1,30 @@
 /**
  * @param {string} password
- * @param {{
- *    length: number,
- *    includeSpecialChars: boolean,
- *    lowerCase: boolean,
- *    upperCase: boolean,
- *    numbers: boolean
- *  }} rulesConfig
- * @returns {{ isSafe: boolean, rules: Record<string, boolean> }}
+ * @param {Object} [config={}]
+ * @param {Object} [config.rules={}]
+ * @param {number} [config.rules.length=8]
+ * @param {boolean} [config.rules.includeSpecialChars=true]
+ * @param {boolean} [config.rules.lowerCase=true]
+ * @param {boolean} [config.rules.upperCase=true]
+ * @param {boolean} [config.rules.numbers=true]
+ * @param {Object} [config.errors={}]
+ * @returns {Object}
+ * @returns {boolean}
+ * @returns {Record<string, boolean>}
+ * @returns {string[]}
  */
-export const isPasswordSafe = (password, rulesConfig = {}) => {
+export const isPasswordSafe = (password, config = {}) => {
+  const { rules = {}, errors = {} } = config
+
   const {
     length = 8,
     includeSpecialChars = true,
     lowerCase = true,
     upperCase = true,
     numbers = true
-  } = rulesConfig
+  } = rules
 
-  const rules = {
+  const rulesCheck = {
     minLength: password.length >= length,
     hasLowerCase: lowerCase ? /[a-z]/.test(password) : true,
     hasUpperCase: upperCase ? /[A-Z]/.test(password) : true,
@@ -28,5 +34,14 @@ export const isPasswordSafe = (password, rulesConfig = {}) => {
       : true
   }
 
-  return { isSafe: Object.values(rules).every(Boolean), rules: rules }
+  const failedRules = Object.keys(rulesCheck).filter(
+    (rule) => !rulesCheck[rule]
+  )
+  const errorMessages = failedRules.map((rule) => errors[rule])
+
+  return {
+    isSafe: failedRules.length === 0,
+    rules: rulesCheck,
+    errors: errorMessages
+  }
 }
